1cd481d7381ffc33b6b1e557d87b178e06bf0de9
Hanz Chiziterem <hancarta23@gmail.com>
2025-05-08 18:25:50 +0100

    Implement Render delay.

--- penforge-ui/src/App.jsx
+++ penforge-ui/src/App.jsx
@@ -1,24 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
-import { html } from '@codemirror/lang-html';
-import { javascript } from '@codemirror/lang-javascript';
-import { css } from '@codemirror/lang-css';
+import { html } from "@codemirror/lang-html";
+import { javascript } from "@codemirror/lang-javascript";
+import { css } from "@codemirror/lang-css";
 
 function App() {
-  const [htmlCode, setHtmlCode] = useState('');
-  const [cssCode, setCssCode] = useState('');
-  const [jsCode, setJsCode] = useState('');
+  const [htmlCode, setHtmlCode] = useState("");
+  const [cssCode, setCssCode] = useState("");
+  const [jsCode, setJsCode] = useState("");
+  const [srcDoc, setSrcDoc] = useState("")
 
+  // Renders to iframe on every 250ms of idleness(delay). 
+  useEffect(()=>{
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+                  <html>
+                    <style>${cssCode}</style>
+                    <body>
+                      ${htmlCode}
+                      <script>
+                        ${jsCode}
+                      </script>
+                    </body>
+                  </html>
+                `);
+    }, 250)
+
+    return () => clearTimeout(timeout); //once renders it clears it.
+  },[htmlCode, cssCode, jsCode]) // this makes sure the cycle repeats.
   return (
     <>
       <div className="pane top-pane">
-        <Editor language={html} displayName="HTML" value={htmlCode} onChange={setHtmlCode} />
-        <Editor language={css} displayName="CSS" value={cssCode} onChange={setCssCode}/>
-        <Editor language={javascript} displayName="JS" value={jsCode} onChange={setJsCode}/>
+        <Editor
+          language={html}
+          displayName="HTML"
+          value={htmlCode}
+          onChange={setHtmlCode}
+        />
+        <Editor
+          language={css}
+          displayName="CSS"
+          value={cssCode}
+          onChange={setCssCode}
+        />
+        <Editor
+          language={javascript}
+          displayName="JS"
+          value={jsCode}
+          onChange={setJsCode}
+        />
       </div>
       <div className="pane">
         <iframe
+          srcDoc={srcDoc}
+          frameBorder={0}
           title="output"
           sandbox="allow-scripts"
           width="100%"
